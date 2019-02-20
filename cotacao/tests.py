87762ac4262c8f23d20c1928138d8a2f08c9686a
jgarciarosa <jgarcia.rosa@gmail.com>
2019-02-20 16:25:52 -0300

    tdd models teste_beep v1.3

--- cotacao/tests.py
+++ cotacao/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import unittest
+from cotacao import models
+from cotacao import views
 
-# Create your tests here.
+class TesteModels(unittest.TestCase):
+
+    def teste_requisicao_igual_duzentos(self):
+        self.assertEqual(200, models.requisicao.status_code)
+
+    def teste_quantidade_dias_igual_sete(self):
+        self.assertEqual(7, len(models.dias))
+
+    def teste_quantidade_cotacoes_igual_sete(self):
+        self.assertEqual(7, len(models.brl_cotacoes))
+        self.assertEqual(7, len(models.ars_cotacoes))
+        self.assertEqual(7, len(models.eur_cotacoes))
+
+
+
+
